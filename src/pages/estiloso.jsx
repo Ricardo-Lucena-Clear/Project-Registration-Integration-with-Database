b70a772feeb800b65c0b309a9027ab876e3e31f2
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-12 12:43:23 -0300

    Modularized CSS

---
+++ src/pages/estiloso.jsx
@@ -0,0 +1,9 @@
+import styles from "./Estiloso.module.css";
+
+export default function Estiloso() {
+    return (
+        <div className={styles.roxo}>
+            <h1>Estilo usando CSS Modules</h1>
+        </div>
+    )
+}
