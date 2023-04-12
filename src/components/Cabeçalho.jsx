20f7d35e5cd014b2efff1c9da81cfa780d221f52
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-12 12:56:19 -0300

    Using Component

---
+++ src/components/Cabeçalho.jsx
@@ -0,0 +1,7 @@
+export default function Cabecalho() {
+    return (
+        <header>
+            <h1>FUndamentos de next.js e react</h1>
+        </header>
+    )
+}
---
+++ src/pages/exemplo.jsx
@@ -0,0 +1,12 @@
+import Cabecalho from "@/components/Cabeçalho";
+
+
+export default function Examplo() {
+    return ( 
+        <div>
+           <Cabecalho/>
+           <Cabecalho/>
+
+        </div>
+    )
+}
